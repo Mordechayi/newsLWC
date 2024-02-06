0f918637566f3f1eaee2716c31cfa368bee0f28f
mordechyi <mordechyi.l@gmail.com.com>
2024-02-06 13:38:05 +0200

    new

---
+++ lwc/newsLWC/newsLWC.js
@@ -0,0 +1,44 @@
+import { LightningElement,track } from 'lwc';
+import retriveNews from "@salesforce/apex/NewsController.retriveNews";
+export default class NewsLWC extends LightningElement {
+    @track resulte = [];
+    @track selectedNews = {};
+    @track isModalOpen = false;
+    get modalClass(){
+        return  `slds-modal ${this.isModalOpen ? "slds-fade-in-open" : ""}`
+    }
+    get modalBackdropClass(){
+        return this.isModalOpen ? "slds-backdrop slds-backdrop--open" : "slds-backdrop";
+    }
+    connectedCallback(){
+        this.fetchNews();
+    }
+    fetchNews(){
+        retriveNews().then(response => {
+            console.log(response);
+            this.formatNewsData(response.articles);
+        }).catch(error => {
+            console.error(error);
+        })
+    }
+    formatNewsData(data){
+        this.resulte = data.map((item, index) => {
+            let id = `new_${index+1}`;
+            let date = new Date(item.publishedAt).toDateString();
+            let name = item.source.name;
+            return {...item, id:id, name:name, date:date}
+        })
+    }
+    showModal(event){
+        let id = event.target.dataset.item;
+        this.resulte.forEach(item => {
+            if(item.id == id){
+                this.selectedNews = {...item};
+            }
+        })
+        this.isModalOpen = true;
+    }
+    closeModal(){
+        this.isModalOpen = false;
+    }
+}
